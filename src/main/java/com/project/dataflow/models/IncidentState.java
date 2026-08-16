dfef4e69636003990bb5b97ff4a9976d0d882d1f
SURBHIGUSAIN <surbhigusain74@gmail.com>
2026-08-16 23:27:36 +0530

    Add IncidentState class with fields for stack trace and logs

--- src/main/java/com/project/dataflow/models/IncidentState.java
+++ src/main/java/com/project/dataflow/models/IncidentState.java
@@ -1,5 +1,26 @@
 package com.project.dataflow.models;
 
+import java.util.List;
+
 public class IncidentState {
+
+    private String stackTrace;
+    private List<String> logs;
+    private PipelineAnalysis pipelineAnalysis;
+
+    public IncidentState(String stackTrace, List<String> logs, PipelineAnalysis pipelineAnalysis) {
+        this.stackTrace = stackTrace;
+        this.logs = logs;
+        this.pipelineAnalysis = pipelineAnalysis;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public List<String> getLogs() {
+        return logs;
+    }
     
+
 }
