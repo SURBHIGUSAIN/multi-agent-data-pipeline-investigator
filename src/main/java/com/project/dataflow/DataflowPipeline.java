ac00e6a840f837a6a530d156dfd21913de7a16b9
SURBHIGUSAIN <surbhigusain74@gmail.com>
2026-08-16 21:00:58 +0530

    Add PCollection input creation to DataflowPipeline

--- src/main/java/com/project/dataflow/DataflowPipeline.java
+++ src/main/java/com/project/dataflow/DataflowPipeline.java
@@ -1,10 +1,27 @@
 package com.project.dataflow;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
 
-public class DataflowPipeline{
+public class DataflowPipeline {
+
     public static void main(String[] args) {
         Pipeline pipeline = Pipeline.create();
+
+        PCollection<String> input = pipeline.apply("Create String Data", Create.of("Alice", "Bob", "Charlie", "David"));
+
+        PCollection<String> upperCaseOutput = input.apply("Uppercase", ParDo.of(new DoFn<String, String> () {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            String input = c.element();
+            String upperCaseOutput = input.toUpperCase();
+            c.output(upperCaseOutput);
+        }
+    }));
+
+        pipeline.run().waitUntilFinish();
     }
-    
 }
