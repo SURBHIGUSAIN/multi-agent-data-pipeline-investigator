2db89563efa0c1448a2dffe540c90d55010242ce
SURBHIGUSAIN <surbhigusain74@gmail.com>
2026-08-16 22:28:36 +0530

    Refactor DataflowPipeline to use Map<String, String> for input data and enhance error handling

--- src/main/java/com/project/dataflow/DataflowPipeline.java
+++ src/main/java/com/project/dataflow/DataflowPipeline.java
@@ -1,13 +1,15 @@
 package com.project.dataflow;
 
+import java.util.Map;
+
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.beam.sdk.io.TextIO;
 
 public class DataflowPipeline {
     private static final Logger logger = LoggerFactory.getLogger(DataflowPipeline.class);
@@ -15,18 +17,25 @@ public class DataflowPipeline {
     public static void main(String[] args) {
         Pipeline pipeline = Pipeline.create();
 
-        PCollection<String> input = pipeline.apply("Create String Data", Create.of("Alice", "Bob", "Charlie", "CRASH"));
+        PCollection<Map<String, String>> input = pipeline.apply("Create Map Data", Create.of(
+            Map.of("name", "Alice", "age", "25"),
+            Map.of("name", "Bob", "age", "31"),
+            Map.of("name", "Charlie", "age", "INVALID")
+        ));
 
-        PCollection<String> upperCaseOutput = input.apply("Uppercase", ParDo.of(new DoFn<String, String> () {
-        @ProcessElement
-        public void processElement(ProcessContext c) {
-            String input = c.element();
-            if ("CRASH".equals(input)) {
-                logger.error("Encountered CRASH input, throwing exception.");
-                throw new RuntimeException("Simulated crash for input: " + input);
-            }
-            String upperCaseOutput = input.toUpperCase();
-            logger.info("Input: " + input + ", Uppercase Output: " + upperCaseOutput);
+        PCollection<String> upperCaseOutput = input.apply("Uppercase", ParDo.of(new DoFn<Map<String, String>, String>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                Map<String, String> input = c.element();
+                String name = input.get("name");
+                String age = input.get("age");
+                if ("CRASH".equals(name)) {
+                    logger.error("Encountered CRASH input, throwing exception.");
+                    throw new RuntimeException("Simulated crash for input: " + name);
+                }
+                String upperCaseOutput = name.toUpperCase();
+                Integer ageInt = Integer.parseInt(age);
+            logger.info("Name: " + name + ", Age: " + ageInt);
             c.output(upperCaseOutput);
         }
     }));
