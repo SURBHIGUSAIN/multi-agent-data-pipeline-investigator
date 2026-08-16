19f077bbd7bb3ddf49b8ed3971f5aece80198e12
SURBHIGUSAIN <surbhigusain74@gmail.com>
2026-08-16 23:54:04 +0530

    Add OpenAI Java SDK dependency and create PipelineIncidentAgent class for incident analysis

---
+++ src/main/java/com/project/dataflow/agents/PipelineIncidentAgent.java
@@ -0,0 +1,11 @@
+package com.project.dataflow.agents;
+
+import com.project.dataflow.models.IncidentState;
+import com.project.dataflow.models.PipelineAnalysis;
+
+public class PipelineIncidentAgent {
+
+    public PipelineAnalysis analyzeIncident(IncidentState incidentState) {
+        
+    }
+}
