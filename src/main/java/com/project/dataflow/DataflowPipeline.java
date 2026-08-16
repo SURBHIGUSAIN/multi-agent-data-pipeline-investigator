9b46abd7fda6b2ca03d1549da7054c055c408754
SURBHIGUSAIN <surbhigusain74@gmail.com>
2026-08-16 18:52:19 +0530

    Initial project commit

---
+++ src/main/java/com/project/dataflow/DataflowPipeline.java
@@ -0,0 +1,10 @@
+package com.project.dataflow;
+
+import org.apache.beam.sdk.Pipeline;
+
+public class DataflowPipeline{
+    public static void main(String[] args) {
+        Pipeline pipeline = Pipeline.create();
+    }
+    
+}
